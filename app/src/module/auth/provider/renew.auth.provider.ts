b18a2aeda1e17e0141ef8691cc7141302fa75452
Igor Morais <igor.morais@hollowsoft.com>
2026-03-20 17:47:45 -0300

    refactor on authentication mechanism

--- app/src/module/auth/provider/renew.auth.provider.ts
+++ app/src/module/auth/provider/renew.auth.provider.ts
@@ -4,19 +4,29 @@ import { JwtService } from '@nestjs/jwt'
 
 import { isNil } from 'lodash'
 
-import type { UserRepository } from '@/module/user/repository/user.repository'
+import { UserRepository } from '@/module/user/repository/user.repository'
 
 import { TokenResponse } from '../auth.response'
 
 export class RenewAuthProvider {
+  private readonly TokenSecret: string
+  private readonly TokenExpire: number
+  private readonly TokenRenewSecret: string
+  private readonly TokenRenewExpire: number
+
   constructor(
-    private readonly jwt: JwtService,
+    private readonly token: JwtService,
     private readonly config: ConfigService,
     private readonly repository: UserRepository
-  ) {}
+  ) {
+    this.TokenSecret = this.config.getOrThrow<string>('TOKEN_SECRET')
+    this.TokenExpire = this.config.getOrThrow<number>('TOKEN_EXPIRE')
+    this.TokenRenewSecret = this.config.getOrThrow<string>('TOKEN_RENEW_SECRET')
+    this.TokenRenewExpire = this.config.getOrThrow<number>('TOKEN_RENEW_EXPIRE')
+  }
 
-  async run(mail: string): Promise<TokenResponse> {
-    const user = await this.repository.find({ mail })
+  async run(phone: string): Promise<TokenResponse> {
+    const user = await this.repository.find({ phone })
 
     if (isNil(user)) {
       throw new BadRequestException()
@@ -26,21 +36,27 @@ export class RenewAuthProvider {
       sub: user.id,
       user: {
         id: user.id,
-        mail: user.mail,
+        phone: user.phone,
         role: user.role
       }
     }
 
-    const token = this.jwt.sign(param, {
-      secret: this.config.get<string>('TOKEN_SECRET'),
-      expiresIn: this.config.get<number>('TOKEN_EXPIRE')
-    })
+    const token = {
+      hash: this.token.sign(param, {
+        secret: this.TokenSecret,
+        expiresIn: this.TokenExpire
+      }),
+      expire: this.TokenExpire
+    }
 
-    const renew = this.jwt.sign(param, {
-      secret: this.config.get<string>('TOKEN_RENEW_SECRET'),
-      expiresIn: this.config.get<number>('TOKEN_RENEW_EXPIRE')
-    })
+    const renew = {
+      hash: this.token.sign(param, {
+        secret: this.TokenRenewSecret,
+        expiresIn: this.TokenRenewExpire
+      }),
+      expire: this.TokenRenewExpire
+    }
 
-    return { token, renew }
+    return new TokenResponse(token, renew)
   }
 }
--- app/src/module/auth/provider/verify.auth.provider.ts
+++ app/src/module/auth/provider/verify.auth.provider.ts
@@ -2,6 +2,8 @@ import { BadRequestException } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { JwtService } from '@nestjs/jwt'
 
+import * as bcrypt from 'bcrypt'
+import type { Cache as CacheManager } from 'cache-manager'
 import { isNil } from 'lodash'
 
 import { UserRepository } from '@/module/user/repository/user.repository'
@@ -9,15 +11,33 @@ import { UserRepository } from '@/module/user/repository/user.repository'
 import { TokenResponse } from '../auth.response'
 
 export class VerifyAuthProvider {
+  private readonly TokenSecret: string
+  private readonly TokenExpire: number
+  private readonly TokenRenewSecret: string
+  private readonly TokenRenewExpire: number
+
   constructor(
-    private readonly jwt: JwtService,
+    private readonly token: JwtService,
+    private readonly store: CacheManager,
     private readonly config: ConfigService,
     private readonly repository: UserRepository
-  ) {}
+  ) {
+    this.TokenSecret = this.config.getOrThrow<string>('TOKEN_SECRET')
+    this.TokenExpire = this.config.getOrThrow<number>('TOKEN_EXPIRE')
+    this.TokenRenewSecret = this.config.getOrThrow<string>('TOKEN_RENEW_SECRET')
+    this.TokenRenewExpire = this.config.getOrThrow<number>('TOKEN_RENEW_EXPIRE')
+  }
 
-  async run(mail: string, _code: string): Promise<TokenResponse> {
-    // TODO: check code
-    const user = await this.repository.find({ mail })
+  async run(phone: string, code: string): Promise<TokenResponse> {
+    const hash = await this.store.get<string>(phone)
+
+    const value = await bcrypt.compare(code, hash ?? '')
+
+    if (value === false) {
+      throw new BadRequestException()
+    }
+
+    const user = await this.repository.find({ phone })
 
     if (isNil(user)) {
       throw new BadRequestException()
@@ -27,21 +47,27 @@ export class VerifyAuthProvider {
       sub: user.id,
       user: {
         id: user.id,
-        mail: user.mail,
+        phone: user.phone,
         role: user.role
       }
     }
 
-    const token = this.jwt.sign(param, {
-      secret: this.config.get<string>('TOKEN_SECRET'),
-      expiresIn: this.config.get<number>('TOKEN_EXPIRE')
-    })
+    const token = {
+      hash: this.token.sign(param, {
+        secret: this.TokenSecret,
+        expiresIn: this.TokenExpire
+      }),
+      expire: this.TokenExpire
+    }
 
-    const renew = this.jwt.sign(param, {
-      secret: this.config.get<string>('TOKEN_RENEW_SECRET'),
-      expiresIn: this.config.get<number>('TOKEN_RENEW_EXPIRE')
-    })
+    const renew = {
+      hash: this.token.sign(param, {
+        secret: this.TokenRenewSecret,
+        expiresIn: this.TokenRenewExpire
+      }),
+      expire: this.TokenRenewExpire
+    }
 
-    return { token, renew }
+    return new TokenResponse(token, renew)
   }
 }
