8f44bfb89f582cf9f3185c043f961d0692bc703a
Igor Morais <igor.morais@hollowsoft.com>
2026-03-22 10:35:45 -0300

    implement organization repository

--- app/src/module/organization/repository/member.ts
+++ app/src/module/organization/repository/member.ts
@@ -2,15 +2,15 @@ import { Prop, Schema } from '@nestjs/mongoose'
 
 import { Types } from 'mongoose'
 
-import type { UserDocument } from '@/module/user/repository/user.schema'
+import type { User } from '@/module/user/repository/user.schema'
 
 import { OrganizationRole } from '../type/organization.role'
 
 @Schema({ _id: false })
 export class Member {
   @Prop({ type: Types.ObjectId, ref: 'User', required: true })
-  user: UserDocument
+  user: User
 
   @Prop({ type: String, enum: OrganizationRole, default: OrganizationRole.MEMBER, required: true })
-  role: OrganizationRole
+  role: OrganizationRole = OrganizationRole.MEMBER
 }
---
+++ app/src/module/organization/repository/organization.repository.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+
+import type { Model, ProjectionType, QueryFilter, QueryOptions, Types } from 'mongoose'
+
+import type { User } from '@/module/user/repository/user.schema'
+
+import { Organization, type OrganizationDocument } from './organization.schema'
+
+import type { OrganizationRole } from '../type/organization.role'
+
+@Injectable()
+export class OrganizationRepository {
+  constructor(@InjectModel(Organization.name) private model: Model<Organization>) {}
+
+  list(
+    query?: QueryFilter<Organization>,
+    projection?: ProjectionType<Organization>,
+    options?: QueryOptions<Organization>
+  ): Promise<OrganizationDocument[]> {
+    return this.model.find(query).projection(projection).options(options).exec()
+  }
+
+  find(
+    query: QueryFilter<Organization>,
+    projection?: ProjectionType<Organization>,
+    options?: QueryOptions<Organization>
+  ): Promise<OrganizationDocument | null> {
+    return this.model.findOne(query, projection, options).exec()
+  }
+
+  create(organization: {
+    name: string
+    member: {
+      user: User
+      role: OrganizationRole
+    }[]
+  }): Promise<OrganizationDocument> {
+    return this.model.create(organization)
+  }
+
+  save(
+    id: Types.ObjectId,
+    organization: { [key: string]: unknown },
+    options?: QueryOptions<Organization>
+  ): Promise<OrganizationDocument | null> {
+    return this.model.findByIdAndUpdate(id, organization, options).exec()
+  }
+
+  async remove(query: QueryFilter<Organization>): Promise<number> {
+    const { deletedCount: amount } = await this.model.deleteOne(query).exec()
+
+    return amount
+  }
+}
