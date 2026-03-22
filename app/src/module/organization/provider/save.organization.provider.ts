c22abcf9f06d1263970f0485abc1a903b6dad447
Igor Morais <igor.morais@hollowsoft.com>
2026-03-22 11:32:03 -0300

    implement save organization provider

---
+++ app/src/module/organization/provider/save.organization.provider.ts
@@ -0,0 +1,31 @@
+import { BadRequestException } from '@nestjs/common'
+
+import { isNil } from 'lodash'
+import { Types } from 'mongoose'
+
+import { SaveOrganizationRequest } from '../organization.request'
+import { OrganizationResponse } from '../organization.response'
+import { OrganizationRepository } from '../repository/organization.repository'
+
+export class SaveOrganizationProvider {
+  constructor(private readonly repository: OrganizationRepository) {}
+
+  async run(id: string, request: SaveOrganizationRequest): Promise<OrganizationResponse> {
+    const { name, document, contact } = request
+
+    const map: { [key: string]: unknown } = {
+      'name': name,
+      'document': document,
+      'contact.mail': contact?.mail,
+      'contact.phone': contact?.phone
+    }
+
+    const organization = await this.repository.save(new Types.ObjectId(id), map)
+
+    if (isNil(organization)) {
+      throw new BadRequestException()
+    }
+
+    return new OrganizationResponse(organization)
+  }
+}
