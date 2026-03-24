8aea362dd36ae273eb1d6e3e57f8a6d652265bf9
Igor Morais <igor.morais@hollowsoft.com>
2026-03-24 17:19:31 -0300

    implement breed module spec

---
+++ app/spec/module/breed/breed.module.spec.ts
@@ -0,0 +1,56 @@
+import { afterAll, beforeAll, describe, expect, test } from 'bun:test'
+
+import type { BreedResponse } from '@/module/breed/breed.response'
+import { Kind } from '@/module/breed/type/kind.enum'
+
+import { encode } from '@/helper/string'
+
+import { Spec } from '../spec'
+
+describe('breed module', async () => {
+  const spec = await Spec.build()
+
+  const value = encode(`${process.env.USER}:${process.env.PASS}`)
+
+  beforeAll(async () => {
+    await spec.start()
+  })
+
+  describe('/breed', async () => {
+    await spec.application
+      .inject()
+      .post('/configuration/breed')
+      .headers({ Authorization: `Basic ${value}` })
+      .end()
+
+    test('should list cat', async () => {
+      const { json } = await spec.application.inject().get('/breed').query({ kind: Kind.CAT }).end()
+
+      const result = json<BreedResponse[]>()
+
+      expect(result).toHaveLength(25)
+
+      expect(result[1]).toMatchObject({
+        id: expect.any(String),
+        name: expect.any(String)
+      })
+    })
+
+    test('should list dog', async () => {
+      const { json } = await spec.application.inject().get('/breed').query({ kind: Kind.DOG }).end()
+
+      const result = json<BreedResponse[]>()
+
+      expect(result).toHaveLength(42)
+
+      expect(result[1]).toMatchObject({
+        id: expect.any(String),
+        name: expect.any(String)
+      })
+    })
+  })
+
+  afterAll(async () => {
+    await spec.stop()
+  })
+})
