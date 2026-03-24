e0ac9c137161e347be0495f41af83c0a9502a039
Igor Morais <igor.morais@hollowsoft.com>
2026-03-24 20:35:02 -0300

    refactor post request

--- app/src/module/post/post.request.ts
+++ app/src/module/post/post.request.ts
@@ -1,21 +1,14 @@
-import { Transform, Type, type TransformFnParams } from 'class-transformer'
+import { Type } from 'class-transformer'
 import {
   IsArray,
   IsBoolean,
-  IsDate,
-  IsEnum,
   IsMongoId,
+  IsNotEmpty,
   IsNumber,
   IsOptional,
   IsString,
-  IsUUID,
-  Length,
-  Min,
-  ValidateNested
+  Min
 } from 'class-validator'
-
-import { Gender } from './type/gender.enum'
-import { Size } from './type/size.enum'
 
 export class GetPostParam {
   @IsMongoId({ message: 'the id is invalid' })
@@ -37,26 +30,6 @@ export class RemovePostParam {
   readonly id: string
 }
 
-class PetRequest {
-  @IsString()
-  @Length(2, 20)
-  @Transform(({ value }: TransformFnParams) => value?.trim())
-  readonly name: string
-
-  @IsDate()
-  @Type(() => Date)
-  readonly birth: Date
-
-  @IsEnum(Size)
-  readonly size: Size
-
-  @IsEnum(Gender)
-  readonly gender: Gender
-
-  @IsMongoId({ message: 'the id is invalid' })
-  readonly breed: string
-}
-
 export class ListPostRequest {
   @IsNumber()
   @Min(1)
@@ -68,24 +41,41 @@ export class ListPostRequest {
   @Type(() => Number)
   readonly amount: number
 
-  @IsMongoId({ message: 'the id is invalid' })
-  @IsOptional()
+  @IsMongoId({ message: 'the location is invalid' })
   readonly location: string
 }
 
-export class SavePostRequest {
-  @IsString()
-  @Length(4, 400)
-  @Transform(({ value }: TransformFnParams) => value?.trim())
-  readonly description: string
-
+export class CreatePostRequest {
   @IsArray()
-  @IsUUID(4, { each: true })
+  @IsString({ each: true })
+  @IsNotEmpty({ each: true })
   readonly image: string[]
 
-  @Type(() => PetRequest)
-  @ValidateNested()
-  readonly pet: PetRequest
+  @IsMongoId({ message: 'the pet is invalid' })
+  readonly pet: string
+
+  @IsMongoId({ message: 'the user is invalid' })
+  @IsOptional()
+  readonly user?: string
+
+  @IsMongoId({ message: 'the organization is invalid' })
+  @IsOptional()
+  readonly organization?: string
+
+  @IsBoolean()
+  @Type(() => Boolean)
+  readonly publish: boolean
+}
+
+export class SavePostRequest {
+  @IsArray()
+  @IsString({ each: true })
+  @IsNotEmpty({ each: true })
+  readonly image: string[]
+
+  @IsBoolean()
+  @Type(() => Boolean)
+  readonly publish: boolean
 }
 
 export class SavePublishPostRequest {
