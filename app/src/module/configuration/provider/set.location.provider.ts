977963dcc88c1c8d50080b7d75140c268941bb37
Igor Morais <igor.morais@hollowsoft.com>
2026-03-24 17:47:36 -0300

    some minor adjustment on location module

--- app/src/module/configuration/provider/set.location.provider.ts
+++ app/src/module/configuration/provider/set.location.provider.ts
@@ -1,3 +1,4 @@
+import { LocationResponse } from '@/module/location/location.response'
 import { LocationRepository } from '@/module/location/repository/location.repository'
 
 import location from '../location.json'
@@ -5,9 +6,11 @@ import location from '../location.json'
 export class SetLocationProvider {
   constructor(private readonly repository: LocationRepository) {}
 
-  async run(): Promise<void> {
+  async run(): Promise<LocationResponse[]> {
     await this.repository.remove()
 
-    await this.repository.create(location)
+    const result = await this.repository.create(location)
+
+    return result.map((e) => new LocationResponse(e))
   }
 }
