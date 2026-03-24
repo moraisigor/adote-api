00072bc26792ffe670f1e0f62fd0609f5b48ff06
Igor Morais <igor.morais@hollowsoft.com>
2026-03-24 20:32:01 -0300

    refactor remove post provider

--- app/src/module/post/provider/remove.post.provider.ts
+++ app/src/module/post/provider/remove.post.provider.ts
@@ -1,19 +1,14 @@
 import { NotFoundException } from '@nestjs/common'
 
-import { Types } from 'mongoose'
-
-import { PostRepository } from '../repository/post.repository'
+import type { PostRepository } from '../repository/post.repository'
 
 export class RemovePostProvider {
-  readonly empty = 0
+  private readonly empty = 0
 
   constructor(private readonly repository: PostRepository) {}
 
-  async run(id: string, user: string): Promise<void> {
-    const amount = await this.repository.remove({
-      _id: id,
-      user: new Types.ObjectId(user)
-    })
+  async run(id: string): Promise<void> {
+    const amount = await this.repository.remove({ _id: id })
 
     if (this.empty === amount) {
       throw new NotFoundException()
