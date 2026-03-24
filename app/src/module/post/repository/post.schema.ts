9579edd22000140213eec1a5bafbe3055aa5c27d
Igor Morais <igor.morais@hollowsoft.com>
2026-03-24 19:50:51 -0300

    update post schema

--- app/src/module/post/repository/post.schema.ts
+++ app/src/module/post/repository/post.schema.ts
@@ -2,31 +2,11 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 
 import { Types, type HydratedDocument } from 'mongoose'
 
-import type { BreedDocument } from '@/module/breed/repository/breed.schema'
+import type { OrganizationDocument } from '@/module/organization/repository/organization.schema'
+import type { PetDocument } from '@/module/pet/repository/pet.schema'
 import type { UserDocument } from '@/module/user/repository/user.schema'
 
-import { Gender } from '../type/gender.enum'
-import { Size } from '../type/size.enum'
-
 export type PostDocument = HydratedDocument<Post>
-
-@Schema({ _id: false })
-export class Pet {
-  @Prop({ type: String, required: true })
-  name: string
-
-  @Prop({ type: Date, required: true })
-  birth: Date
-
-  @Prop({ type: String, enum: Size, required: true })
-  size: Size
-
-  @Prop({ type: String, enum: Gender, required: true })
-  gender: Gender
-
-  @Prop({ type: Types.ObjectId, ref: 'Breed', required: true })
-  breed: BreedDocument
-}
 
 @Schema({
   id: true,
@@ -35,18 +15,21 @@ export class Pet {
 })
 export class Post {
   @Prop({ type: String, required: true })
-  description: string
+  name: string
 
   @Prop({ type: [String], required: true })
   image: string[]
 
-  @Prop({ type: Pet, required: true })
-  pet: Pet
+  @Prop({ type: Types.ObjectId, ref: 'Pet', required: true })
+  pet: PetDocument
 
-  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
-  user: UserDocument
+  @Prop({ type: Types.ObjectId, ref: 'User' })
+  user?: UserDocument
 
-  @Prop({ type: Boolean, default: true })
+  @Prop({ type: Types.ObjectId, ref: 'Organization' })
+  organization?: OrganizationDocument
+
+  @Prop({ type: Boolean, required: true, default: true })
   publish: boolean = true
 }
 
