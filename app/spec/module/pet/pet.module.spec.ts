8bfc7d7fad073d502a3894e741513c8cf608462b
Igor Morais <igor.morais@hollowsoft.com>
2026-03-26 14:38:32 -0300

    add pet module spec

---
+++ app/spec/module/pet/pet.module.spec.ts
@@ -0,0 +1,54 @@
+import { Kind } from '@/module/breed/type/kind.enum'
+import type { PetResponse } from '@/module/pet/pet.response'
+import { Gender } from '@/module/pet/type/gender'
+import { Size } from '@/module/pet/type/size'
+
+import { Spec } from '../spec'
+
+describe('pet module', async () => {
+  const spec = await Spec.build()
+
+  beforeAll(async () => {
+    await spec.start()
+
+    await spec.authenticate()
+
+    await spec.breed(Kind.DOG)
+  })
+
+  describe('/pet', () => {
+    test('should create pet', async () => {
+      const { token } = spec.authorization
+
+      const { id: breed } = spec.result.breed.find((e) => e.name.toLowerCase() === 'buldogue inglês') ?? {
+        breed: ''
+      }
+
+      const { json } = await spec.application
+        .inject()
+        .post('/pet')
+        .headers({ Authorization: `Bearer ${token?.hash}` })
+        .body({
+          name: 'Oreo',
+          size: Size.MEDIUM,
+          gender: Gender.MALE,
+          breed: breed
+        })
+        .end()
+
+      const response = json<PetResponse>()
+
+      expect(response).toMatchObject({
+        id: expect.any(String),
+        name: expect.any(String),
+        size: expect.any(String),
+        gender: expect.any(String),
+        breed: expect.any(Object)
+      })
+    })
+  })
+
+  afterAll(async () => {
+    await spec.stop()
+  })
+})
