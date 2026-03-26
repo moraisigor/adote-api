5e0f4a1b725241f431efc94c2c6876dd9b96b0ad
Igor Morais <igor.morais@hollowsoft.com>
2026-03-26 12:52:01 -0300

    update location module spec

--- app/spec/module/location/location.module.spec.ts
+++ app/spec/module/location/location.module.spec.ts
@@ -2,34 +2,26 @@ import { afterAll, beforeAll, describe, expect, test } from 'bun:test'
 
 import type { LocationResponse } from '@/module/location/location.response'
 
-import { encode } from '@/helper/string'
-
 import { Spec } from '../spec'
 
 describe('location module', async () => {
   const spec = await Spec.build()
 
-  const value = encode(`${process.env.USER}:${process.env.PASS}`)
-
   beforeAll(async () => {
     await spec.start()
 
-    await spec.application
-      .inject()
-      .post('/configuration/location')
-      .headers({ Authorization: `Basic ${value}` })
-      .end()
+    await spec.location()
   })
 
   describe('/location', async () => {
     test('should list location', async () => {
       const { json } = await spec.application.inject().get('/location').query({ search: 'Recife' }).end()
 
-      const result = json<LocationResponse[]>()
+      const response = json<LocationResponse[]>()
 
-      expect(result).toHaveLength(1)
+      expect(response).toHaveLength(1)
 
-      expect(result).toMatchObject([
+      expect(response).toMatchObject([
         {
           id: expect.any(String),
           city: 'Recife',
