68ec8c0c415aac155855fbcdef43948769b8d837
Igor Morais <igor.morais@hollowsoft.com>
2026-03-26 12:50:21 -0300

    update breed module spec

--- app/spec/module/breed/breed.module.spec.ts
+++ app/spec/module/breed/breed.module.spec.ts
@@ -3,34 +3,26 @@ import { afterAll, beforeAll, describe, expect, test } from 'bun:test'
 import type { BreedResponse } from '@/module/breed/breed.response'
 import { Kind } from '@/module/breed/type/kind.enum'
 
-import { encode } from '@/helper/string'
-
 import { Spec } from '../spec'
 
 describe('breed module', async () => {
   const spec = await Spec.build()
 
-  const value = encode(`${process.env.USER}:${process.env.PASS}`)
-
   beforeAll(async () => {
     await spec.start()
 
-    await spec.application
-      .inject()
-      .post('/configuration/breed')
-      .headers({ Authorization: `Basic ${value}` })
-      .end()
+    await spec.breed()
   })
 
   describe('/breed', () => {
     test('should list cat', async () => {
       const { json } = await spec.application.inject().get('/breed').query({ kind: Kind.CAT }).end()
 
-      const result = json<BreedResponse[]>()
+      const response = json<BreedResponse[]>()
 
-      expect(result).toHaveLength(25)
+      expect(response).toHaveLength(25)
 
-      expect(result[1]).toMatchObject({
+      expect(response[1]).toMatchObject({
         id: expect.any(String),
         name: expect.any(String)
       })
@@ -39,11 +31,11 @@ describe('breed module', async () => {
     test('should list dog', async () => {
       const { json } = await spec.application.inject().get('/breed').query({ kind: Kind.DOG }).end()
 
-      const result = json<BreedResponse[]>()
+      const response = json<BreedResponse[]>()
 
-      expect(result).toHaveLength(42)
+      expect(response).toHaveLength(42)
 
-      expect(result[1]).toMatchObject({
+      expect(response[1]).toMatchObject({
         id: expect.any(String),
         name: expect.any(String)
       })
