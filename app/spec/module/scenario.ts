5eb609a110cc739b7e751d56348e2d10fc276069
Igor Morais <igor.morais@hollowsoft.com>
2026-04-06 20:24:47 -0300

    add scenario

---
+++ app/spec/module/scenario.ts
@@ -0,0 +1,9 @@
+import type { NestFastifyApplication } from '@nestjs/platform-fastify'
+
+import { encode } from '@/helper/string'
+
+export class Scenario {
+  public readonly BASIC = encode(`${process.env.USER}:${process.env.PASS}`)
+
+  constructor(private readonly application: NestFastifyApplication) {}
+}
