12522d805b387af422c002dce8268170beae5cce
Igor Morais <igor.morais@hollowsoft.com>
2026-04-06 18:47:32 -0300

    update spec

--- app/spec/module/spec.ts
+++ app/spec/module/spec.ts
@@ -5,60 +5,30 @@ import { JwtModule } from '@nestjs/jwt'
 import { MongooseModule } from '@nestjs/mongoose'
 import { PassportModule } from '@nestjs/passport'
 import { FastifyAdapter, type NestFastifyApplication } from '@nestjs/platform-fastify'
-import { Test, type TestingModule } from '@nestjs/testing'
+import { Test } from '@nestjs/testing'
 
 import { MongoMemoryServer } from 'mongodb-memory-server'
 
 import { AuthModule } from '@/module/auth/auth.module'
-import type { TokenResponse } from '@/module/auth/auth.response'
-import type { Token } from '@/module/auth/type/token'
 import { BreedModule } from '@/module/breed/breed.module'
-import type { BreedResponse } from '@/module/breed/breed.response'
 import { ConfigurationModule } from '@/module/configuration/configuration.module'
+import { FavModule } from '@/module/fav/fav.module'
 import { HealthModule } from '@/module/health/health.module'
 import { LocationModule } from '@/module/location/location.module'
-import type { LocationResponse } from '@/module/location/location.response'
 import { MessageModule } from '@/module/message/message.module'
 import { OrganizationModule } from '@/module/organization/organization.module'
 import { PetModule } from '@/module/pet/pet.module'
-import type { PetResponse } from '@/module/pet/pet.response'
-import { Gender } from '@/module/pet/type/gender'
-import { Size } from '@/module/pet/type/size'
 import { PostModule } from '@/module/post/post.module'
 import { UserModule } from '@/module/user/user.module'
 
-import { encode } from '@/helper/string'
-
+import { Scenario } from './scenario'
 import { RouteConfig } from '@/router.config.factory'
 
-export type Result = {
-  pet?: PetResponse
-  // support
-  breed: BreedResponse[]
-  location: LocationResponse[]
-}
-
-export type Authorization = {
-  basic: string
-  // auth
-  token?: Token
-  renew?: Token
-}
-
 export class Spec {
-  public result: Result = {
-    breed: [],
-    location: []
-  }
-
-  public authorization: Authorization = {
-    basic: encode(`${process.env.USER}:${process.env.PASS}`)
-  }
-
   constructor(
-    readonly module: TestingModule,
-    readonly application: NestFastifyApplication,
-    private readonly repository: MongoMemoryServer
+    private readonly repository: MongoMemoryServer,
+    readonly scenario: Scenario,
+    readonly application: NestFastifyApplication
   ) {}
 
   public static build = async () => {
@@ -69,6 +39,7 @@ export class Spec {
         AuthModule,
         BreedModule,
         ConfigurationModule,
+        FavModule,
         HealthModule,
         LocationModule,
         MessageModule,
@@ -92,103 +63,14 @@ export class Spec {
             uri: repository.getUri()
           })
         })
-      ],
-      providers: []
+      ]
     }).compile()
 
     const application = module.createNestApplication<NestFastifyApplication>(new FastifyAdapter())
 
-    return new Spec(module, application, repository)
-  }
+    const scenario = new Scenario(application)
 
-  // support
-  async breed() {
-    const { basic } = this.authorization
-
-    const { json } = await this.application
-      .inject()
-      .post('/configuration/breed')
-      .headers({ Authorization: `Basic ${basic}` })
-      .end()
-
-    this.result.breed = json<BreedResponse[]>()
-  }
-
-  // support
-  async location() {
-    const { basic } = this.authorization
-
-    const { json } = await this.application
-      .inject()
-      .post('/configuration/location')
-      .headers({ Authorization: `Basic ${basic}` })
-      .end()
-
-    this.result.location = json<LocationResponse[]>()
-  }
-
-  async pet() {
-    const { token } = this.authorization
-
-    const { id: breed } = this.result.breed.find((e) => e.name.toLowerCase() === 'buldogue inglês') ?? {
-      breed: ''
-    }
-
-    const { json } = await this.application
-      .inject()
-      .post('/pet')
-      .headers({ Authorization: `Bearer ${token?.hash}` })
-      .body({
-        name: 'Oreo',
-        size: Size.MEDIUM,
-        gender: Gender.MALE,
-        breed: breed
-      })
-      .end()
-
-    this.result.pet = json<PetResponse>()
-  }
-
-  async user() {
-    const { token } = this.authorization
-
-    const { id: location } = this.result.location.find((e) => e.city.toLowerCase() === 'recife') ?? {
-      location: ''
-    }
-
-    await this.application
-      .inject()
-      .put('/user')
-      .headers({ Authorization: `Bearer ${token?.hash}` })
-      .body({
-        name: 'Name',
-        image: 'image.jpg',
-        contact: {
-          mail: 'mail@example.com',
-          phone: '+5599999999999',
-          social: 'https://example.com/name'
-        },
-        location: location
-      })
-      .end()
-  }
-
-  async authenticate() {
-    await this.application.inject().post('/auth').body({ phone: '+5599999999999' }).end()
-
-    const { json } = await this.application
-      .inject()
-      .post('/auth/verify')
-      .body({
-        phone: '+5599999999999',
-        code: '999999'
-      })
-      .end()
-
-    const result = json<TokenResponse>()
-
-    this.authorization.token = result.token
-    this.authorization.renew = result.renew
+    return new Spec(repository, scenario, application)
   }
 
   async start() {
