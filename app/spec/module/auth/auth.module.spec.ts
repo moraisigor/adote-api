6222fe1a79cc10006fcfef655418c41c9d947b64
Igor Morais <igor.morais@hollowsoft.com>
2026-04-06 18:40:07 -0300

    update auth module spec

--- app/spec/module/auth/auth.module.spec.ts
+++ app/spec/module/auth/auth.module.spec.ts
@@ -1,59 +1,114 @@
 import { afterAll, beforeAll, describe, expect, test } from 'bun:test'
 
-import type { AuthResponse, TokenResponse } from '@/module/auth/auth.response'
+import { TokenResponse, type AuthResponse } from '@/module/auth/auth.response'
 
 import { Spec } from '../spec'
 
+type Result = {
+  auth?: AuthResponse
+  token?: TokenResponse
+}
+
 describe('auth module', async () => {
   const spec = await Spec.build()
+
+  const result: Result = {}
 
   beforeAll(async () => {
     await spec.start()
   })
 
-  describe('/auth', () => {
-    test('should auth user', async () => {
-      const { json } = await spec.application
-        .inject()
-        .post('/auth')
-        .body({
-          phone: '+5599999999999'
-        })
-        .end()
-
-      const response = json<AuthResponse>()
-
-      expect(response).toMatchObject({
-        id: expect.any(String),
-        key: expect.any(String),
+  // /auth
+  test('should start auth', async () => {
+    const { json } = await spec.application
+      .inject()
+      .post('/auth')
+      .body({
         phone: '+5599999999999'
       })
+      .end()
+
+    const response = json<AuthResponse>()
+
+    result.auth = response
+
+    expect(response).toMatchObject({
+      id: expect.any(String),
+      key: expect.any(String),
+      phone: '+5599999999999'
     })
   })
 
-  describe('/auth/verify', () => {
-    test('should verify user', async () => {
-      const { json } = await spec.application
-        .inject()
-        .post('/auth/verify')
-        .body({
-          phone: '+5599999999999',
-          code: '999999'
-        })
-        .end()
+  // /auth/key
+  test('should auth user with key', async () => {
+    const { auth: { key } = { key: '' } } = result
 
-      const response = json<TokenResponse>()
+    const { json } = await spec.application.inject().post('/auth/key').headers({ Key: key }).end()
 
-      expect(response).toMatchObject({
-        token: {
-          hash: expect.any(String),
-          expire: Number(process.env.TOKEN_EXPIRE)
-        },
-        renew: {
-          hash: expect.any(String),
-          expire: Number(process.env.TOKEN_RENEW_EXPIRE)
-        }
+    const response = json<TokenResponse>()
+
+    expect(response).toMatchObject({
+      token: {
+        hash: expect.any(String),
+        expire: Number(process.env.TOKEN_EXPIRE)
+      },
+      renew: {
+        hash: expect.any(String),
+        expire: Number(process.env.TOKEN_RENEW_EXPIRE)
+      }
+    })
+  })
+
+  // /auth/verify
+  test('should auth user with code', async () => {
+    const { auth: { phone } = { phone: '' } } = result
+
+    const { json } = await spec.application
+      .inject()
+      .post('/auth/verify')
+      .body({
+        phone,
+        code: '999999'
       })
+      .end()
+
+    const response = json<TokenResponse>()
+
+    result.token = response
+
+    expect(response).toMatchObject({
+      token: {
+        hash: expect.any(String),
+        expire: Number(process.env.TOKEN_EXPIRE)
+      },
+      renew: {
+        hash: expect.any(String),
+        expire: Number(process.env.TOKEN_RENEW_EXPIRE)
+      }
+    })
+  })
+
+  // /auth/renew
+  test('should renew auth', async () => {
+    const { token: { renew: { hash } } = { renew: { hash: '' } } } = result
+
+    const { json } = await spec.application
+      .inject()
+      .post('/auth/renew')
+      .headers({ Authorization: `Bearer ${hash}` })
+      .end()
+
+    const response = json<TokenResponse>()
+
+    expect(response).toMatchObject({
+      token: {
+        hash: expect.any(String),
+        expire: Number(process.env.TOKEN_EXPIRE)
+      },
+      renew: {
+        hash: expect.any(String),
+        expire: Number(process.env.TOKEN_RENEW_EXPIRE)
+      }
     })
   })
 
