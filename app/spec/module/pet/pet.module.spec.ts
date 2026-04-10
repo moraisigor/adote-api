f5b7e68459a68207baedc1443f7cf2a4ec6885c8
Igor Morais <igor.morais@hollowsoft.com>
2026-04-10 17:03:50 -0300

    update pet module spec

--- app/spec/module/pet/pet.module.spec.ts
+++ app/spec/module/pet/pet.module.spec.ts
@@ -1,53 +1,61 @@
+import type { BreedResponse } from '@/module/breed/breed.response'
+import { Kind } from '@/module/breed/type/kind.enum'
 import type { PetResponse } from '@/module/pet/pet.response'
 import { Gender } from '@/module/pet/type/gender'
 import { Size } from '@/module/pet/type/size'
 
 import { Spec } from '../spec'
 
+type Result = {
+  breed?: BreedResponse[]
+}
+
 describe('pet module', async () => {
   const spec = await Spec.build()
+
+  const result: Result = {}
 
   beforeAll(async () => {
     await spec.start()
 
-    // support
-    await spec.breed()
+    await spec.scenario.pet()
 
-    await spec.authenticate()
+    const { json } = await spec.application.inject().get('/breed').query({ kind: Kind.DOG }).end()
+
+    result.breed = json<BreedResponse[]>()
   })
 
-  describe('/pet', () => {
-    test('should create pet', async () => {
-      const { token } = spec.authorization
+  // /pet
+  test('should create pet', async () => {
+    const {
+      authorization: { token: { hash } = { hash: '' } }
+    } = spec.scenario
 
-      const { id: breed } = spec.result.breed.find((e) => e.name.toLowerCase() === 'buldogue inglês') ?? {
-        breed: ''
-      }
+    const { id: breed } = result.breed?.find((e) => e.name === 'Buldogue Inglês') ?? { id: null }
 
-      const { json } = await spec.application
-        .inject()
-        .post('/pet')
-        .headers({ Authorization: `Bearer ${token?.hash}` })
-        .body({
-          name: 'Oreo',
-          size: Size.MEDIUM,
-          gender: Gender.MALE,
-          breed: breed
-        })
-        .end()
-
-      const response = json<PetResponse>()
-
-      expect(response).toMatchObject({
-        id: expect.any(String),
+    const { json } = await spec.application
+      .inject()
+      .post('/pet')
+      .headers({ Authorization: `Bearer ${hash}` })
+      .body({
         name: 'Oreo',
         size: Size.MEDIUM,
         gender: Gender.MALE,
-        breed: {
-          id: expect.any(String),
-          name: 'Buldogue Inglês'
-        }
+        breed: breed
       })
+      .end()
+
+    const response = json<PetResponse>()
+
+    expect(response).toMatchObject({
+      id: expect.any(String),
+      name: 'Oreo',
+      size: Size.MEDIUM,
+      gender: Gender.MALE,
+      breed: {
+        id: expect.any(String),
+        name: 'Buldogue Inglês'
+      }
     })
   })
 
