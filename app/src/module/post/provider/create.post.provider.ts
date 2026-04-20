6d36c57baafd17112746872ca48e577840d140a3
Igor Morais <igor.morais@hollowsoft.com>
2026-04-20 16:31:56 -0300

    check permission on create post provider

--- app/src/module/post/provider/create.post.provider.ts
+++ app/src/module/post/provider/create.post.provider.ts
@@ -1,31 +1,47 @@
 import { Types } from 'mongoose'
 
+import type { PermissionProvider } from '@/module/permission/provider'
+
 import { CreatePostRequest } from '../post.request'
 import { PostResponse } from '../post.response'
 import { PostRepository } from '../repository/post.repository'
 
 export class CreatePostProvider {
-  constructor(private readonly repository: PostRepository) {}
+  constructor(
+    private readonly provider: PermissionProvider,
+    private readonly repository: PostRepository
+  ) {}
 
-  private build(request: CreatePostRequest, user: string) {
-    const { image, pet, location, organization, publish } = request
+  private build(request: CreatePostRequest, param: { [key: string]: unknown }) {
+    const { image, pet, location, publish } = request
 
-    const post = {
-      image,
-      pet: new Types.ObjectId(pet),
-      location: new Types.ObjectId(location),
-      publish
-    }
-
-    if (organization) {
-      return Object.assign(post, { organization: new Types.ObjectId(organization) })
-    }
-
-    return Object.assign(post, { user: new Types.ObjectId(user) })
+    return Object.assign(
+      {
+        image,
+        pet: new Types.ObjectId(pet),
+        location: new Types.ObjectId(location),
+        publish
+      },
+      param
+    )
   }
 
-  async run(request: CreatePostRequest, user: string): Promise<PostResponse> {
-    const post = await this.repository.create(this.build(request, user))
+  async run(request: CreatePostRequest, current: string): Promise<PostResponse> {
+    const { organization } = request
+
+    if (organization) {
+      const permission = await this.provider.organization.run(current, organization)
+
+      if (permission) {
+        const post = await this.repository.create(
+          this.build(request, { organization: new Types.ObjectId(organization) })
+        )
+
+        return new PostResponse(post)
+      }
+    }
+
+    const post = await this.repository.create(this.build(request, { user: new Types.ObjectId(current) }))
 
     return new PostResponse(post)
   }
