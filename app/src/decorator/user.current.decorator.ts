ec1118d9ece82214f42c2906d5dcd5b3ccd7bfa5
Igor Morais <igor.morais@hollowsoft.com>
2026-04-20 08:33:42 -0300

    add type to request on user decorator

--- app/src/decorator/user.current.decorator.ts
+++ app/src/decorator/user.current.decorator.ts
@@ -1,7 +1,9 @@
 import { createParamDecorator, type ExecutionContext } from '@nestjs/common'
 
+import type { FastifyRequest } from 'fastify'
+
 export const UserCurrent = createParamDecorator((_: unknown, context: ExecutionContext) => {
-  const { user } = context.switchToHttp().getRequest()
+  const { user } = context.switchToHttp().getRequest<FastifyRequest>()
 
   return user
 })
