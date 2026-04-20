8255dce4b4933ae7574c44a4d9d898214dc313d2
Igor Morais <igor.morais@hollowsoft.com>
2026-04-20 16:32:57 -0300

    check permission on publish post provider

--- app/src/module/post/provider/publish.post.provider.ts
+++ app/src/module/post/provider/publish.post.provider.ts
@@ -1,25 +1,49 @@
-import { NotFoundException } from '@nestjs/common'
+import { BadRequestException, NotFoundException } from '@nestjs/common'
 
 import { isNil } from 'lodash'
 import { Types } from 'mongoose'
+
+import type { PermissionProvider } from '@/module/permission/provider'
+
+import { Permission } from '@/helper/permission'
 
 import { PublishPostResponse } from '../post.response'
 import { PostRepository } from '../repository/post.repository'
 
 export class PublishPostProvider {
-  constructor(private readonly repository: PostRepository) {}
+  constructor(
+    private readonly provider: PermissionProvider,
+    private readonly repository: PostRepository
+  ) {}
 
-  async run(id: string, publish: boolean, user: string): Promise<PublishPostResponse> {
-    const map: { [key: string]: unknown } = {
-      publish
-    }
-
-    const post = await this.repository.save(new Types.ObjectId(id), map, { returnDocument: 'after' })
+  async run(id: string, publish: boolean, current: string): Promise<PublishPostResponse> {
+    const post = await this.repository.find({ _id: id })
 
     if (isNil(post)) {
-      throw new NotFoundException()
+      throw new BadRequestException()
     }
 
-    return new PublishPostResponse(post)
+    const { user, organization } = post
+
+    const permission = await new Permission(current, this.provider).run(
+      user as Types.ObjectId,
+      organization as Types.ObjectId
+    )
+
+    if (permission) {
+      const result = await this.repository.save(
+        new Types.ObjectId(id),
+        { publish },
+        { returnDocument: 'after' }
+      )
+
+      if (isNil(result)) {
+        throw new BadRequestException()
+      }
+
+      return new PublishPostResponse(post)
+    }
+
+    throw new BadRequestException()
   }
 }
