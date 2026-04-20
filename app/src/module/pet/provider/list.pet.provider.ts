7aab37b421dd12a2130f180a4e599556ac1df2d5
Igor Morais <igor.morais@hollowsoft.com>
2026-04-20 12:56:44 -0300

    check for permission on list pet provider

--- app/src/module/pet/provider/list.pet.provider.ts
+++ app/src/module/pet/provider/list.pet.provider.ts
@@ -1,33 +1,43 @@
-import { Types } from 'mongoose'
+import { Types, type QueryFilter } from 'mongoose'
+
+import { PermissionProvider } from '@/module/permission/provider'
 
 import { ListPetRequest } from '../pet.request'
 import { PetResponse } from '../pet.response'
 import { PetRepository } from '../repository/pet.repository'
+import type { Pet } from '../repository/pet.schema'
 
 export class ListPetProvider {
-  constructor(private readonly repository: PetRepository) {}
+  constructor(
+    private readonly provider: PermissionProvider,
+    private readonly repository: PetRepository
+  ) {}
 
-  private query(request: ListPetRequest, user: string) {
-    const { organization } = request
-
-    if (organization) {
-      return {
-        organization: new Types.ObjectId(organization)
-      }
-    }
-
-    return {
-      user: new Types.ObjectId(user)
-    }
-  }
-
-  async run(request: ListPetRequest, user: string): Promise<PetResponse[]> {
+  private async list(request: ListPetRequest, query: QueryFilter<Pet>): Promise<PetResponse[]> {
     const { page, amount } = request
 
     const skip = (page - 1) * amount
 
-    const list = await this.repository.list(skip, amount, this.query(request, user))
+    const list = await this.repository.list(skip, amount, query)
 
     return list.map((e) => new PetResponse(e))
+  }
+
+  async run(request: ListPetRequest, current: string): Promise<PetResponse[]> {
+    const { organization } = request
+
+    if (organization) {
+      const permission = await this.provider.organization.run(current, organization)
+
+      if (permission) {
+        return await this.list(request, {
+          organization: new Types.ObjectId(organization)
+        })
+      }
+    }
+
+    return await this.list(request, {
+      user: new Types.ObjectId(current)
+    })
   }
 }
