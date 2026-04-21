8a654f65219561d1587bc94e0a99d0718a91897c
Igor Morais <igor.morais@hollowsoft.com>
2026-04-21 08:54:04 -0300

    inject all provider on image module

--- app/src/module/image/provider/index.ts
+++ app/src/module/image/provider/index.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
 
 import { RemoveImageProvider } from './remove.image.provider'
 import { SaveImageProvider } from './save.image.provider'
 
 @Injectable()
 export class ImageProvider {
-  readonly save: SaveImageProvider
-  readonly remove: RemoveImageProvider
-
-  constructor(private readonly config: ConfigService) {
-    this.save = new SaveImageProvider(this.config)
-    this.remove = new RemoveImageProvider(this.config)
-  }
+  constructor(
+    readonly save: SaveImageProvider,
+    readonly remove: RemoveImageProvider
+  ) {}
 }
--- app/src/module/image/provider/save.image.provider.ts
+++ app/src/module/image/provider/save.image.provider.ts
@@ -1,8 +1,10 @@
+import { Injectable } from '@nestjs/common'
 import type { ConfigService } from '@nestjs/config'
 
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
+@Injectable()
 export class SaveImageProvider {
   private readonly client: S3Client
 
