e820431d800988bac59d15e9f9fdf956885f9bfd
Igor Morais <igor.morais@hollowsoft.com>
2026-04-21 09:38:56 -0300

    fix image module

--- app/src/module/image/image.module.ts
+++ app/src/module/image/image.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 
 import { ImageProvider } from './provider'
+import { RemoveImageProvider } from './provider/remove.image.provider'
+import { SaveImageProvider } from './provider/save.image.provider'
 
 @Module({
   exports: [ImageProvider],
-  providers: [ImageProvider]
+  providers: [ImageProvider, SaveImageProvider, RemoveImageProvider]
 })
 export class ImageModule {}
