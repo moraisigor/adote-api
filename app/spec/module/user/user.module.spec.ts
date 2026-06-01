5bffd3e970d5f9e2c014e59a379c43a08ec0eb76
Igor Morais <igor.morais@hollowsoft.com>
2026-06-01 09:56:14 -0300

    some minor adjustments

--- app/spec/module/user/user.module.spec.ts
+++ app/spec/module/user/user.module.spec.ts
@@ -208,3 +208,48 @@ describe('user module', () => {
     })
   })
 })
+
+describe('user module', () => {
+  describe('as manager', async () => {
+    const spec = await Spec.build()
+
+    beforeAll(async () => {
+      await spec.start()
+
+      await spec.scenario.manager()
+    })
+
+    // get /user
+    describe('/user', () => {
+      test('should list user', async () => {
+        const {
+          authorization: { token: { hash } = { hash: '' } }
+        } = spec.scenario
+
+        const { statusCode: status, json } = await spec.application
+          .inject()
+          .get('/user')
+          .headers({ Authorization: `Bearer ${hash}` })
+          .end()
+
+        const response = json<UserResponse[]>()
+
+        expect(status).toBe(HttpStatus.OK)
+
+        expect(response).toHaveLength(1)
+
+        expect(response).toMatchObject([
+          {
+            id: expect.any(String),
+            phone: '+5599999999999',
+            name: 'Dev'
+          }
+        ])
+      })
+    })
+
+    afterAll(async () => {
+      await spec.stop()
+    })
+  })
+})
