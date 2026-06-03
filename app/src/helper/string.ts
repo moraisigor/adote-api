b03b8b5e58b63d8ae09b00bc8bfdbb28b54f8634
Igor Morais <igor.morais@hollowsoft.com>
2026-06-03 15:28:58 -0300

    minor improvement on search location

--- app/src/helper/string.ts
+++ app/src/helper/string.ts
@@ -1,3 +1,5 @@
 export const encode = (value: string): string => btoa(value)
 
 export const decode = (value: string): string => atob(value)
+
+export const escape = (value: string): string => value.replace(/[.+*^?$|\\()[\]{}-]/g, '\\$&')
--- app/src/module/location/provider/search.location.provider.ts
+++ app/src/module/location/provider/search.location.provider.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common'
 
+import { escape } from '@/helper/string'
+
 import { LocationResponse } from '../location.response'
 import { LocationRepository } from '../repository/location.repository'
 
@@ -8,12 +10,16 @@ export class SearchLocationProvider {
   constructor(private readonly repository: LocationRepository) {}
 
   async run(search: string): Promise<LocationResponse[]> {
-    const list = await this.repository.list({
-      city: {
-        $regex: search,
-        $options: 'i'
-      }
-    })
+    const list = await this.repository.list(
+      {
+        city: {
+          $regex: `^${escape(search)}`,
+          $options: 'i'
+        }
+      },
+      {},
+      { limit: 10 }
+    )
 
     return list.map((e) => new LocationResponse(e))
   }
